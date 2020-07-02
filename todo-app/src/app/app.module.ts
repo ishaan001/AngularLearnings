3a5758dffb63dd200a6c9b7d1e0aa9ba88e447fb
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-03 02:35:42 +0530

    files and relevent folders created

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './components/todos/todos.component';
+import { TodoFormComponent } from './components/todo-form/todo-form.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    TodoFormComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
