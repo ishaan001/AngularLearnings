5d23422d3451c0d382c1739dd87367a3829fe211
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-05 19:57:29 +0530

    todo app completed

--- todo-app/src/app/app.module.ts
+++ todo-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { HeaderComponent } from './layout/header/header.component';
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
+    FormsModule,  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- todo-app/src/app/components/todo-form/todo-form.component.ts
+++ todo-app/src/app/components/todo-form/todo-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from './../../model/Todo';
+import { v4 as uuidv4 } from "uuid";
+import { TodoService } from './../../service/todo.service'
 
 @Component({
   selector: 'app-todo-form',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFormComponent implements OnInit {
 
-  constructor() { }
+  todoTitle :string;
+  constructor(private todoService : TodoService) { }
 
   ngOnInit(): void {
+  }
+
+  handleAdd(){
+    const todo : Todo ={
+      id : uuidv4(),
+      title : this.todoTitle,
+      isCompleted :false,
+      date : new Date(),
+    };
+
+    this.todoService.addTodos(todo);
+    this.todoTitle = '';
   }
 
 }
