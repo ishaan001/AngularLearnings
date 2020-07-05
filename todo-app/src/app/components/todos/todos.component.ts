c0b51e47f5625e19de840f210cbfdc5ac506854d
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-05 16:38:37 +0530

    todo service changes added

--- todo-app/src/app/components/todos/todos.component.ts
+++ todo-app/src/app/components/todos/todos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/service/todo.service';
+import { Todo } from './../../model/Todo';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { ThrowStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-todos',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
-  constructor() { }
+  faTrashAlt = faTrashAlt;
+  todos : Todo[];
+  constructor(private todoService : TodoService) { }
 
   ngOnInit(): void {
+    this.todoService.getTodos().subscribe(
+      todos => { this.todos = todos})
+  }
+
+  changeTodoStatus(todo: Todo){
+    this.todoService.changeStatus(todo);
+  }
+
+  deleteTodo(todo : Todo){
+    this.todoService.deleteTodo(todo);
   }
 
 }
--- todo-app/src/app/service/todo.service.ts
+++ todo-app/src/app/service/todo.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
-
+import { of } from 'rxjs';
+import { Todo } from './../model/Todo';
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todos : Todo[];
+  constructor() {
+    this.todos = [
+    {
+      id : '1',
+      title : 'learn java',
+      isCompleted : true,
+      date : new Date()
+    },
+    {
+      id : '2',
+      title : 'learn DS and ALGO',
+      isCompleted : false,
+      date : new Date()
+    },
+    {
+      id : '3',
+      title : 'learn Angular',
+      isCompleted : false,
+      date : new Date()
+    },
+  ];
+   }
 
-  constructor() { }
+   getTodos = () => {
+     return of(this.todos);
+   }
+
+   addTodos(todo : Todo){
+    this.todos.push(todo);
+   }
+
+   changeStatus(todo : Todo){
+     this.todos.map(singleTodo => {
+       if(singleTodo.id === todo.id){
+         todo.isCompleted = !todo.isCompleted;
+       }
+     })
+   }
+
+   deleteTodo(todo : Todo){
+     const index = this.todos.findIndex( x => x.id == todo.id);
+     this.todos.slice(index, 1);
+    }
+
 }
