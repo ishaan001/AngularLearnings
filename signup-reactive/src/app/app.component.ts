b03cfdd5f48be6a49940b362c12e30cc4658525f
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-07 02:01:54 +0530

    validatos, formbuilder and formgroup added

--- signup-reactive/src/app/app.component.ts
+++ signup-reactive/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'signup-reactive';
+
+  registerForm : FormGroup;
+  submited : false;
+
+  ngOnInit(){
+
+  }
+
 }
