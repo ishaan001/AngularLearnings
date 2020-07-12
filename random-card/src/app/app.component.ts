bb08030c792b2d89f0033d4f50102329e426e57d
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-12 09:15:10 +0530

    the details which fetched in service are injected in the mail component.ts file

--- random-card/src/app/app.component.ts
+++ random-card/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+import { ToastrService } from 'ngx-toastr';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'random-card';
+  // instead of using any we can create an interface and use that also
+  user : any;
+
+  constructor(
+              private userService : UserService, 
+              private toastr: ToastrService, 
+              
+             ){ }
+
+  ngOnInit(){
+    this.userService.getUser().subscribe(
+      (user :any) => {
+        console.log(user);
+        this.user = user.results[2];
+      },
+      (err)=>{
+        this.toastr.error(err.status, "OOPs");
+      },
+    )
+  }
 }
--- random-card/src/app/services/user.service.ts
+++ random-card/src/app/services/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+//using whatever we imported in the modules.ts
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  private url = 'https://randomuser.me/api';
+  constructor(private http : HttpClient) { }
+  
+  getUser(){
+    return this.http.get(this.url);
+  }
+
 }
