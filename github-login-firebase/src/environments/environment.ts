950e3ccd8c15432b292fe132db9c7c3b00a17efd
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-14 03:38:38 +0530

    firbase config key added in environment

--- github-login-firebase/src/environments/environment.ts
+++ github-login-firebase/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebase : {
+    apiKey: "AIzaSyAKNlrOERwwb5iomouZIsXHobO6HrJ-3TQ",
+    authDomain: "github-login-firebase-51b96.firebaseapp.com",
+    databaseURL: "https://github-login-firebase-51b96.firebaseio.com",
+    projectId: "github-login-firebase-51b96",
+    storageBucket: "github-login-firebase-51b96.appspot.com",
+    messagingSenderId: "79684791680",
+    appId: "1:79684791680:web:ac6f0ca3197154db546565"
+  }
 };
 
 /*
