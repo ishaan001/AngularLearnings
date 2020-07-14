8f793fe8d9aa9d67f83d53a8b13308a3849c85ee
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-15 02:44:37 +0530

    authentication function added in service via firbase

--- github-login-firebase/src/app/services/auth.service.ts
+++ github-login-firebase/src/app/services/auth.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private auth: AngularFireAuth) { }
+  
+  signUp(email: string, password: string){
+    //return observable which we can subscribe with rxJS
+    return this.auth.createUserWithEmailAndPassword(email, password);
+  }
+
+  signIn(email: string, password: string){
+    //return observable which we can subscribe with rxJS
+    return this.auth.signInWithEmailAndPassword(email, password);
+
+  }
+
+  getUser(){
+    // returned by firebase if there is the successfull sigIn.
+    return this.auth.authState;
+  }
+
+  signOut(){
+    return this.auth.signOut();
+  }
 }
