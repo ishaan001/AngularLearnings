4bc6c34e1896da62bc31944e71832150f62f2b6e
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-16 05:21:16 +0530

    header component created

--- github-login-firebase/src/app/layout/header/header.component.ts
+++ github-login-firebase/src/app/layout/header/header.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router} from '@angular/router'
+
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  email = null;
+  constructor(
+      private auth : AuthService,
+      private router : Router,
+      private toastr :ToastrService
+  ) { 
+    auth.getUser().subscribe((user)=>{
+      this.email = user?.email;
+    })
+  }
 
   ngOnInit(): void {
+  } 
+
+  async handlesSignOut(){
+    try {
+      //res == response 
+      const res = await this.auth.signOut();
+      this.router.navigateByUrl('/signin');
+      this.toastr.info('Login Again to Continue ');
+      this.email = null;  
+    } catch (error) {
+      this.toastr.error('something is wrong');  
+    }
   }
 
 }
