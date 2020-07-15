870d8a23e35c0d58340dc545fdaf32e4ae202524
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-16 03:59:03 +0530

    worked with github API to retrive user and repos details

--- github-login-firebase/src/app/services/github.service.ts
+++ github-login-firebase/src/app/services/github.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+//https://api.github.com/users/ishaan001
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  constructor(private  http: HttpClient) { }
+
+  gitUserDetails(userName:string){
+    return this.http.get(`https://api.github.com/users/${userName}`)
+  }
+
+  getRepos(repoUrl:string){
+    return this.http.get(repoUrl);
+  }
 }
