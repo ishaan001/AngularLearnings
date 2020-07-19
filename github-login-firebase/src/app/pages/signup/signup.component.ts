9f948383fcb772e5e058cd382d729a098c2ebcec
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-19 21:07:53 +0530

    sighup modules added

--- github-login-firebase/src/app/pages/signup/signup.component.ts
+++ github-login-firebase/src/app/pages/signup/signup.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
-@Component({
+
+@Component({  
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService :AuthService,
+    private router : Router,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
   }
+
+  
 
 }
