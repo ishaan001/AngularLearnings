a400dd5b4f0ab704df88991a2ae59ce7c2106827
ishaan001 <ishaanveerdadhwal@gmail.com>
2020-07-20 07:53:38 +0530

    was not creating build folder so made changes to env.prod.ts file

--- github-login-firebase/src/environments/environment.prod.ts
+++ github-login-firebase/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyAKNlrOERwwb5iomouZIsXHobO6HrJ-3TQ",
+    authDomain: "github-login-firebase-51b96.firebaseapp.com",
+    databaseURL: "https://github-login-firebase-51b96.firebaseio.com",
+    projectId: "github-login-firebase-51b96",
+    storageBucket: "github-login-firebase-51b96.appspot.com",
+    messagingSenderId: "79684791680",
+    appId: "1:79684791680:web:ac6f0ca3197154db546565"
+  }
 };
